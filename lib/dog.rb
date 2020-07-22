382a30d645d3b213e43d3c4d0e29bb296cb33d07
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-22 17:15:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,27 @@
 class Dog
 
- def initialize(name)
-   @name = name
- end
+  def initialize(name)
+    @name = name
+  end
  
- def name=name
-   @name = name
- end
+  def name=name
+    @name = name
+  end
 
-def name
-  @name
-end
+  def name
+    @name
+  end
 
  
- def breed= (breed)
-   @breed = breed
- end
+  def breed= (breed)
+    @breed = breed
+  end
  
- def breed 
-   if @breed == nil
-     return "Mutt"
+  def breed 
+    if @breed == nil
+    return "Mutt"
    end
+ end
 
  
 end 
