e1a951521d157ce16d999d0d24926038415a950b
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-22 17:12:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,27 @@ class Dog
  attr_accessor :name, :breed
  end
  
+ def initialize(name, breed)
+   @name = name
+   @breed = breed
+ end
+ 
+ def name=name
+   @name = name
+ end
+
+def name
+  @name
+end
+
+ 
  def breed= (breed)
    @breed = breed
  end
  
  def breed 
    if @breed == nil
-     return @breed["Mutt"]
+     return "Mutt"
    end
 
  
